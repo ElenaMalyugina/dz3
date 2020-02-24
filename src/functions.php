f82b334e97f0980ad1207b89b8e9b53f717dc39e
Elena <malyugina.el@yandex.ru>
2020-02-25 02:11:16 +0300

    xml

---
+++ src/functions.php
@@ -0,0 +1,49 @@
+<?php
+//task1
+function printOrder(){
+    $fileXMLContent = file_get_contents('data.xml');
+    $fileContent = new SimpleXMLElement($fileXMLContent);
+   
+    echo('<h4>Order</h4>');
+    echo('<table>');
+        echo('<tr>');
+            echo("<td>Order Number</td>");
+            echo("<td>{$fileContent->attributes()->PurchaseOrderNumber}</td>");
+        echo('</tr>');
+        echo('<tr>');
+            echo("<td>Order Date</td>");
+            echo("<td>{$fileContent->attributes()->OrderDate}</td>");
+        echo('</tr>');
+    echo('</table>');
+    echo('<br>');
+    echo('<table>');
+        foreach($fileContent->Address as $address){
+            echo('<tr>');
+                echo("<td>{$address->attributes()->Type} address</td>");
+            echo('</tr>');    
+            foreach($address as $key=>$addressItem){
+                echo('<tr>');
+                    echo("<td>{$key}</td>");
+                    echo("<td>{$addressItem}</td>");
+                echo('</tr>');
+            }                
+        }
+    echo('</table>');
+    echo('<br>');
+    echo("<p>{$fileContent->DeliveryNotes}</p>");
+    echo('<table>');
+        foreach($fileContent->Items->Item as $item){
+            echo('<tr>');
+                echo('<td>Part Number</td>');
+                echo("<td>{$item->attributes()->PartNumber}</td>");
+            echo('</tr>');    
+                foreach($item as $key=>$value){
+                    echo('<tr>');
+                        echo("<td>{$key}</td>");
+                        echo("<td>{$value}</td>");
+                    echo('</tr>');   
+                }
+            echo('</tr>');    
+        }
+    echo('</table>');
+}

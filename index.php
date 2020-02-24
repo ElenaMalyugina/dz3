98e949eb11fb788e55b9acbe5cfaa42c3e585000
Elena <malyugina.el@yandex.ru>
2020-02-25 02:30:41 +0300

    стили

--- index.php
+++ index.php
@@ -1,4 +1,16 @@
-<?php
-require('./src/functions.php');
-
-printOrder();
+<!--Это сделано для подключения стилей к листу заказа-->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        require('./src/functions.php');
+        printOrder();
+    ?>    
+</body>
+</html>
